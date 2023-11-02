a3f3baebe8254584b9947034458762c9a81c0479
Sja30 <144677436+Sja30@users.noreply.github.com>
2023-11-02 15:44:46 +0300

    Update 10.pas

--- 10.pas
+++ 10.pas
@@ -2,25 +2,25 @@ program z10;
 var a: array[1..20] of integer;
 i, j: integer;
 begin
-for i := 1 to 20 do
+for i:= 1 to 20 do
 a[i]:=random(-50,50);
 Writeln('Исходный массив:');
-for i:= 1 to 20 do
+for i := 1 to 20 do
 Write(a[i], ' ');
 Writeln;
 j := 1;
-for i:= 1 to 20 do
+for i := 1 to 20 do
 begin
 if a[i]>= 0 then
 begin
-a[j]:= a[i];
+a[j] := a[i];
 Inc(j);
 end;
 end;
-for i := j to 20 do
-a[i]:= 0;
+for i := j to 19 do
+a[i]:= a[i+1];
 Writeln('Измененный массив:');
-for i := 1 to 20 do
+for i := 1 to j-1 do
 Write(a[i], ' ');
 Writeln;
 end.
